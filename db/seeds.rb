6e98faaf22e7bf7e342519ae1b826e5082736975
Hvdvyve <hvdvyve@gmail.com>
2021-12-18 18:49:22 +0000

    Make seeders

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Equipment.create!([
+    { name: 'Beemer', description: 'Some kind of projector.', status: 'working'}, 
+    { name: 'Cable', description: 'Internet connections with cable connection.', status: 'working'}, 
+    { name: 'Lamp', description: 'Movable lamp', status: 'working'}
+    ])
+
+RoomOrDesk.create!([
+    { wing: 'A', floor: 1, number: 'A12', places: 1, type: 'Desk' }
+    { wing: 'B', floor: 1, number: 'A1', places: 2, type: 'Conference room' }
+])
+
+RoomOrDeskEquipment.create!([
+    { RoomOrDeskId: 1, EquipmentId:1 }
+    { RoomOrDeskId: 1, EquipmentId:2 }
+])
