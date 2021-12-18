c8caff51e917fd57b3b7e42fb39bbd6bfcb1b420
Hvdvyve <hvdvyve@gmail.com>
2021-12-18 20:18:46 +0000

    Nest the API routes

---
+++ app/controllers/api/V1/room_or_desk_equipments_controller.rb
@@ -0,0 +1,6 @@
+module Api
+    module V1
+        class RoomOrDeskEquipmentController < ApplicationController
+        end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+namespace 'api' do
+  namespace 'v1' do
+    resources :equipment
+    resources :room_or_desks
+    resources :room_or_desk_equipments
+  end
+end
