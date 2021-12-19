28b56c5595a5c33b3fbe1e061aec698e021393e8
Hvdvyve <hvdvyve@gmail.com>
2021-12-19 13:13:16 +0000

    Make post & delete api calls for equipment

--- app/controllers/api/V1/equipment_controller.rb
+++ app/controllers/api/V1/equipment_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::EquipmentController < ApplicationController
+    skip_before_action :verify_authenticity_token
 
     #GET /equipment
     def index
-        @equipment = Equipment.all
         render json: ([
             { name: 'Beemer', description: 'Some kind of projector.'}, 
             { name: 'Cable', description: 'Internet connections with cable connection.'}, 
@@ -12,10 +12,17 @@ class Api::V1::EquipmentController < ApplicationController
 
     #GET /equipment/:id
     def show
-        @equipment = Equipment.find(params[:id])
-        render json: { name: 'Beemer', description: 'Some kind of projector.'}
+        render json: { id: 1, name: 'Beemer', description: 'Some kind of projector.'}
     end
 
-    
+    #POST /equipment
+    def create
+        render json: { message: 'Equipment created successfully.'}, status: 200    
+    end
+
+    #DELETE /equipment/:id
+    def destroy
+        render json: { message: 'Equipment deleted successfully.' }, status: 200 
+    end
 
 end
--- app/controllers/api/V2/equipment_controller.rb
+++ app/controllers/api/V2/equipment_controller.rb
@@ -1,4 +1,5 @@
 class Api::V2::EquipmentController < ApplicationController
+    skip_before_action :verify_authenticity_token
 
     #GET /equipment
     def index
@@ -12,4 +13,30 @@ class Api::V2::EquipmentController < ApplicationController
         render json: @equipment
     end
 
+    #POST /equipment
+    def create
+        @equipment = Equipment.new(fact_params)
+        if @equipment.save
+            render json: { message: 'Equipment created successfully.'}, status: 200
+        else
+            render error: { error: 'Cannot create Equipment' }, status: 400
+        end
+    end
+
+    #DELETE /equipment/:id
+    def destroy
+        @equipment = Equipment.find(params[:id])
+        if @equipment
+            @equipment.destroy
+            render json: { message: 'Equipment deleted successfully.' }, status: 200 
+        else
+            render error: { error: 'Cannot delete Equipment' }, status: 400
+        end
+    end
+
+    private
+
+    def fact_params
+        params.permit(:name, :description)
+    end
 end
